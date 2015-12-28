b48c92ebabb96e09e33a3ce5f9d67a7170c2f6e2
yzhizai <duansf@ihep.ac.cn>
2015-12-28 10:27:59 +0800

    adjust the format of the comments

--- nii_sform_to_quaternion.m
+++ nii_sform_to_quaternion.m
@@ -1,5 +1,5 @@
 function quaternion = nii_sform_to_quaternion(nii)
-%Syntax: QUATERNION = NII_SFORM_TO_QUATERNION(NII)
+%Syntax: quaternion = NII_SFORM_TO_QUATERNION(nii)
 %
 %  Input:
 %    nii - output from load_untouch_header_only.m
@@ -8,9 +8,9 @@ function quaternion = nii_sform_to_quaternion(nii)
 %    quaternion - a 4*4 matrix
 %
 %See also: LOAD_UNTOUCH_HEADER_ONLY
-% Author: Shaofeng Duan (duansf@ihep.ac.cn)
-% Institute of High Energy Physics 
-% Dec 2015
+%Author: Shaofeng Duan (duansf@ihep.ac.cn)
+%Institute of High Energy Physics 
+%Dec 2015
 quaternion = [nii.hist.srow_x; ...
                 nii.hist.srow_y; ...
                 nii.hist.srow_z; ...
